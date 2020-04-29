3eb2f53a867076b47c467a07dce6076bafa5ffcc
Timo Tuomi <ttuomi@gmail.com>
2020-04-29 14:18:52 +0300

    4.2

--- index.js
+++ index.js
@@ -1,45 +1,13 @@
-require('dotenv').config()
+//require('dotenv').config()
+const app = require('./app') // varsinainen Express-sovellus
+
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URL
-// const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
---
+++ models/blog.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+const logger = require('../utils/logger')
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+  
+  const Blog = mongoose.model('Blog', blogSchema)
+  
+  module.exports = Blog
